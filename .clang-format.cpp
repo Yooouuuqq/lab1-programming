6badffa661386ab0d81fac96330ad7acb598f929
Ибатуллин Амир <amiribatullin@mail.ru>
2026-09-25 19:11:07 +0300

    Добавлено форматирование: clang-format и black

---
+++ .clang-format.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+auto square(int x) -> int {
+    return x *x;
+}
+int main() {
+    auto number{7};
+    auto result = square(number);
+
+    std::cout <<"Число:"<<number<<std::endl;
+    std::cout <<"Квадрат:" << result << std::endl;
+
+    return 0
+
+}
--- mainn.cpp
+++ mainn.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 
-auto square(int x) -> int {
-    return x *x;
-}
+auto square(int x) -> int { return x * x; }
 int main() {
     auto number{7};
     auto result = square(number);
 
-    std::cout <<"Число:"<<number<<std::endl;
-    std::cout <<"Квадрат:" << result << std::endl;
+    std::cout << "Число:" << number << std::endl;
+    std::cout << "Квадрат:" << result << std::endl;
 
     return 0;
-
 }
